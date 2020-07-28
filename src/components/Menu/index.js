bb82b7d8a42c21bc117aa2aa0913e6a5979f0b3a
lzricardo <lzricardo.ecomp@gmail.com>
2020-07-27 21:39:12 -0300

    :package::sparkles: Create components, added styled components and create data mock.

---
+++ src/components/Menu/index.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Logo from '../../assets/img/Logo.png';
+import './Menu.css'
+import Button from "../Button";
+// import ButtonLink from "./components/ButtonLink";
+
+function Menu() {
+  return (
+    <nav className="Menu">
+      <a href="/">
+        <img className="Logo" src={Logo} alt="Nakflix logo"/>
+      </a>
+
+      {/*<ButtonLink href="/" className="ButtonLink">*/}
+      {/*  Novo vídeo*/}
+      {/*</ButtonLink>*/}
+      <Button as="a" className="ButtonLink" href="/">
+        Novo vídeo
+      </Button>
+    </nav>
+  );
+}
+
+export default Menu;
